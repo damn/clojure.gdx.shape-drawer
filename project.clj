2455c4a6a6b3b50a1bf7589b68b97136a6a26ab8
Michael Sappler <michaelsappler@gmail.com>
2025-09-29 21:33:56 +0530

    simplify

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject gdl.graphics.shape-drawer "2.5.0"
+(defproject space.earlygrey.shape-drawer "0.1"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [com.badlogic.gdx "1.13.5"]
                  [space.earlygrey/shapedrawer "2.5.0"]]
   :codox {:source-uri "https://github.com/damn/com.badlogic.gdx.graphics.g2d.freetype/blob/main/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
