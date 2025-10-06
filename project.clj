cffdb1ab256ca9ac17d18268bed329712e42d495
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 16:16:42 +0530

    fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject space.earlygrey.shape-drawer "0.1"
+(defproject space.earlygrey.shape-drawer "2.5.0"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [space.earlygrey/shapedrawer "2.5.0"]]
